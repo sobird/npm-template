9876a3aba62149843345036d2d1c53271386c321
sobird <i@sobird.me>
2024-04-14 11:57:59 +0800

    update

--- src/register.js
+++ src/register.js
@@ -1,4 +1,4 @@
 import { register } from 'node:module';
 import { pathToFileURL } from 'node:url';
 
-register('./ts-esm-loader-with-tsconfig-paths.js', pathToFileURL('./'));
+register(pathToFileURL('./ts-esm-loader-with-tsconfig-paths.js'), pathToFileURL('./'));
