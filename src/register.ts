2107580850b1a49d12fa5790abb885533f6db1e6
sobird <i@sobird.me>
2024-04-14 12:14:00 +0800

    resolve register file

---
+++ src/register.ts
@@ -0,0 +1,10 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable @typescript-eslint/naming-convention */
+import { dirname, resolve } from 'node:path';
+import { register } from 'node:module';
+import { fileURLToPath, pathToFileURL } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+register(resolve(__dirname, 'ts-esm-loader-with-tsconfig-paths.js'), pathToFileURL('./'));
