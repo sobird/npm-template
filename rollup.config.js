737bddc8216e2cea345026291758c26a1a8359c1
sobird <i@sobird.me>
2025-02-17 01:35:54 +0800

    fix: use @rollup/plugin-terser repleace esbuild & add rollup-plugin-typescript2

--- rollup.config.js
+++ rollup.config.js
@@ -7,17 +7,19 @@
  * @see https://cn.rollupjs.org/configuration-options
  * sobird<i@sobird.me> at 2023/09/28 11:30:37 created.
  */
-import { defineConfig } from 'rollup';
-import { glob } from 'glob';
 import { dirname, relative, extname } from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import external from 'rollup-plugin-peer-deps-external';
-import esbuild from 'rollup-plugin-esbuild';
+
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
-import copy from 'rollup-plugin-copy';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
+import { glob } from 'glob';
+import { defineConfig } from 'rollup';
 import clear from 'rollup-plugin-clear';
+import copy from 'rollup-plugin-copy';
+import external from 'rollup-plugin-peer-deps-external';
+import typescript from 'rollup-plugin-typescript2';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -77,10 +79,15 @@ export default (env) => {
           preferBuiltins: true,
         }),
         commonjs(),
-        esbuild({
-          minify: isProduction,
+        typescript({
+          check: false,
+          // declaration: true,
+          // tsconfig: "./src/tsconfig.json",
+          // noEmitOnError: false,
+          strictRequires: true,
         }),
         json(),
+        terser(),
         copy({
           targets: [
             { src: 'package.json', dest: DIST },
